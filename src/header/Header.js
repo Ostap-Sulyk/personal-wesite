4504cb89b30dba68de248ff0ebb7a9fb261315ed
ostapsulyk <ostapsulyk@gmail.com>
2021-08-19 17:44:47 +0300

    created nav for laptops. need to adjust it for phones and add styling

--- src/header/Header.js
+++ src/header/Header.js
@@ -3,10 +3,11 @@ import './Header.css'
 
 const Header = () => {
     return <div className='nav-container'>
-        <a className='nav-link' href="#">home</a>
-        <a className='nav-link' href="#">something1</a>
-        <a className='nav-link' href="#">something2</a>
-        <a className='nav-link' href="#">something3</a>
+        <div></div>
+        <a href="#" className='nav-link'>home</a>
+        <a href="#" className='nav-link'>projects</a>
+        <a href="#" className='nav-link'>resume</a>
+        <div></div>
     </div>;
 };
 
